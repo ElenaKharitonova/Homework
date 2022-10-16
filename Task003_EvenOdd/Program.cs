74292b4a029d1b04b02c9fe6617d5fe0bdfa1918
KharitonovaElena <lenuciya@mail.ru>
2022-10-16 20:55:38 +0300

    EvenOdd

---
+++ Task003_EvenOdd/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Введите число неравное нулю > ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 0)
+{   
+  System.Console.Write("Вы ввели 0, число не является четным");
+}
+else
+{
+    if (number % 2 == 0)
+    {
+    System.Console.WriteLine($"Число {number} четное"); 
+    }
+    else
+    {
+    System.Console.WriteLine($"Число {number} не является четным");     
+    }
+}
